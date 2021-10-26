f3d9b2a3264013ad9c8e1502acb8c89cbc9b2b83
stratocliff <lordcliff@gmail.com>
2021-10-26 20:45:05 -0300

    Se agrega jQuey
    Ademas se implementa y corrije codigo string literal en tienda

--- js/tienda.js
+++ js/tienda.js
@@ -18,10 +18,6 @@ class productoEnCarrito {
 const serverURL = '../'   //'https://www.almapatisserie.com.ar/'
 const mockImgTiendaPath = 'assets/mock/tienda.json'
 const tiendaImgFullPath = 'assets/img/tienda/'
-const tarjetasContainer = document.querySelector('.tarjetasTienda');
-const shoppingCart = document.querySelector(".shopping-cart")
-const shoppingCartItemsContainer = document.querySelector('.shoppingCartItemsContainer')
-const comprarButton = document.querySelector(".comprarButton")
 var listaProductosEnCarrito //variable para almacenar los productos que hay en el carito
 var listaProductosDeJson //variable para almacenar el array con todos los objetos del JSON
 
@@ -74,17 +70,8 @@ function printTarjetas(data) { //IMPRIME LAS TARJETAS EN EL HTML
         tarjeta.appendChild(img)
         tarjeta.appendChild(miniContenedor)
         itemsContainer.appendChild(tarjeta)
-        tarjetasContainer.appendChild(itemsContainer)
-        /*
-        <article class="tarjeta">
-            <img src="../assets/img/smash.PNG" alt="smash">
-            <div>
-                <h3>Smash con bombones y chocolates</h3>
-                <p>Corazón y base de chocolate blanco / semiamargo o con leche</p>
-                <p>Precio $1700</p>
-                <input type ="button" value="Mas detalles" data-bs-toggle="modal" data-bs-target="#modal1">
-            </div>
-        </article>*/
+        $(".tarjetasTienda").append(itemsContainer)
+
     });
 }
 
@@ -110,7 +97,7 @@ function addToCartClicked(index) {
             return p.titulo === producto.titulo
         })
         if (index > -1) {
-            carrito[index].cantidad += 1
+            carrito[index].cantidad = Number(carrito[index].cantidad) + 1
 
             //swwet alert start
             const Toast = Swal.mixin({
@@ -145,44 +132,81 @@ function addToCartClicked(index) {
 
 function printCarrito() { //
     if (localStorage.getItem("carrito") !== null) {
-        shoppingCart.classList.remove("d-none")
+        $(".shopping-cart").removeClass("d-none")
+        //shoppingCart.classList.remove("d-none")
         printShoppingCart() //funcion para imprimir lo que hay en el carrito
     }
 }
 
 function printShoppingCart() { //imprime lo que hay en el local storage dentro del carrito
     listaObjetos = JSON.parse(localStorage.getItem("carrito"))
-    shoppingCartItemsContainer.innerHTML = ""
+    $(".shoppingCartItemsContainer").html("")
 
     let indice = 0
     listaObjetos.forEach(element => {
+
         const shoppingCartRow = document.createElement("div")
-        const shoppingCartContent = `
-        <div class="row shoppingCartItem">
-            <div class="col-6">
-                <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                    <img src="${serverURL}${tiendaImgFullPath}${element.imagen}" class="shopping-cart-image" alt="${element.alt}">
-                    <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">
-                        ${element.titulo}</h6>
-                </div>
-            </div>
-            <div class="col-2">
-                <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                    <p class="item-price mb-0 shoppingCartItemPrice">$${element.precio}</p>
-                </div>
-            </div>
-            <div class="col-4">
-                <div
-                    class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-                    <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                        value="${element.cantidad}" onchange="cambiarCantidadInput(${indice})">
-                    <button class="btn btn-danger" onClick="removeShoppingCartItem(${indice})" type="button">X</button>
-                </div>
-            </div>
-        </div>
-        `
-        shoppingCartRow.innerHTML = shoppingCartContent
-        shoppingCartItemsContainer.append(shoppingCartRow)
+        shoppingCartRow.classList.add("row")
+
+        const divImgTitulo = document.createElement("div")
+        divImgTitulo.classList.add("col-6")
+
+        const divImgTitulo2 = document.createElement("div")
+        divImgTitulo2.classList.add("d-flex", "align-items-center", "h-100", "border-bottom", "pb-2", "pt-3")
+
+        const img = document.createElement("img")
+        img.classList.add("shopping-cart-image")
+        img.setAttribute("src", `${serverURL}${tiendaImgFullPath}${element.imagen}`)
+        img.setAttribute("alt", `${element.alt}`)
+
+        const titulo = document.createElement("h6")
+        titulo.classList.add("text-truncate", "ml-3", "mb-0")
+        titulo.textContent = `${element.titulo}`
+
+        const divPrecioContainer = document.createElement("div")
+        divPrecioContainer.classList.add("col-2")
+
+        const divPrecio = document.createElement("div")
+        divPrecio.classList.add("d-flex", "align-items-center", "h-100", "border-bottom", "pb-2", "pt-3")
+
+        const pPrecio = document.createElement("p")
+        pPrecio.classList.add("item-price", "mb-0")
+        pPrecio.textContent = `$${element.precio}`
+
+        const divCantidadContainer = document.createElement("div")
+        divCantidadContainer.classList.add("col-4")
+
+        const divCantidad = document.createElement("div")
+        divCantidad.classList.add("d-flex", "justify-content-between", "align-items-center", "h-100", "border-bottom", "pb-2", "pt-3")
+
+        const cantidadInput = document.createElement("input")
+        cantidadInput.classList.add("shopping-cart-quantity-input")
+        cantidadInput.setAttribute("type", "number")
+        cantidadInput.setAttribute("value", `${element.cantidad}`)
+        cantidadInput.setAttribute("onChange", `cambiarCantidadInput(${indice})`)
+
+        const botonBorrar = document.createElement("button")
+        botonBorrar.classList.add("btn", "btn-danger")
+        botonBorrar.setAttribute("type", "button")
+        botonBorrar.setAttribute("onClick", `removeShoppingCartItem(${indice})`)
+        botonBorrar.textContent = "X"
+
+        divImgTitulo2.appendChild(img)
+        divImgTitulo2.appendChild(titulo)
+        divImgTitulo.appendChild(divImgTitulo2)
+
+        divPrecio.appendChild(pPrecio)
+        divPrecioContainer.appendChild(divPrecio)
+
+        divCantidad.appendChild(cantidadInput)
+        divCantidad.appendChild(botonBorrar)
+        divCantidadContainer.appendChild(divCantidad)
+
+        shoppingCartRow.appendChild(divImgTitulo)
+        shoppingCartRow.appendChild(divPrecioContainer)
+        shoppingCartRow.appendChild(divCantidadContainer)
+
+        $(".shoppingCartItemsContainer").append(shoppingCartRow)
 
         updateShoppingCartTotal()
         indice++
@@ -190,17 +214,13 @@ function printShoppingCart() { //imprime lo que hay en el local storage dentro d
 }
 
 function cambiarCantidadInput(indice) {
-    let input = document.querySelector(".shopping-cart-quantity-input")
-    if (input.value <= 0) {
+    let input = document.querySelectorAll(".shopping-cart-quantity-input")
+    if (input[indice].value <= 0) {
         alert("para eliminar un item presione la X")
-        input.value = 1
-    }
-    if (input.value > 10) {
-        alert("Por compras mayoristas comunicarse directamente al numero de WhatsApp")
-        input.value = 10
+        input[indice].value = 1
     }
     carrito = JSON.parse(localStorage.getItem("carrito"))
-    carrito[indice].cantidad = input.value
+    carrito[indice].cantidad = input[indice].value
     localStorage.setItem("carrito", JSON.stringify(carrito))
     updateShoppingCartTotal()
 }
@@ -225,17 +245,15 @@ function removeShoppingCartItem(indice) {
         printShoppingCart()
     } else {
         vaciarCarrito()
-        shoppingCart.classList.add("d-none")
+        $(".shopping-cart").addClass("d-none")
+        //shoppingCart.classList.add("d-none")
     }
-}
-
-function vaciarCarrito() {
-    localStorage.removeItem('carrito');
 }
 
 function comprarButtonClicked() {
     vaciarCarrito()
-    shoppingCart.classList.add("d-none")
+    $(".shopping-cart").addClass("d-none")
+    //shoppingCart.classList.add("d-none")
 
     Swal.fire(
         'Gracias por tu compra!!',
@@ -245,6 +263,9 @@ function comprarButtonClicked() {
     )
 }
 
+function vaciarCarrito() {
+    localStorage.removeItem('carrito');
+}
 /**************************************************************
 *        LOGICA
 ***************************************************************/
@@ -253,22 +274,11 @@ getTarjetas();//obtiene la info de las tajetas y las imprime en el html
 printCarrito();//se fija si hay algo en el carrito (localstorage) si hay lo muestra (llamando a la funcion para imprimir la info correspondiente)
 
 
-var toastElList = [].slice.call(document.querySelectorAll('.toast'))
-var toastList = toastElList.map(function (toastEl) {
-    return new bootstrap.Toast(toastEl)
+$("#comprarButton").click(() => {
+    comprarButtonClicked()
 })
-
-document.querySelector("#comprarButton").addEventListener("click", comprarButtonClicked)
-
-/*
-*/
-
-/* const addToCartButtons = document.querySelectorAll(".addToCart")
-addToCartButtons.forEach((addToCartButton) => {
-    addToCartButton.addEventListener('click', addToCartClicked)
-})*/
-
-
+//Esto es lo mismo pero sin Jquery
+//document.querySelector("#comprarButton").addEventListener("click", comprarButtonClicked)
 
 
 //SCRIPT PARA LIGHTBOX
