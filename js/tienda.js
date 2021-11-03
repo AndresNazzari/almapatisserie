a703a4d79c742508c8b89fa453b4456f31dd397b
stratocliff <lordcliff@gmail.com>
2021-11-03 20:00:43 -0300

    update tienda.js
    se agrega llamada ajax con jquery.

--- js/tienda.js
+++ js/tienda.js
@@ -26,7 +26,17 @@ var listaProductosDeJson //variable para almacenar el array con todos los objeto
 ***************************************************************/
 
 function getTarjetas() { //TRAE EL JSON CON LA INFO DE LAS TARJETAS Y LLAMA A LA FUNCION QUE LAS IMPRIME EN EL HTML
-    fetch(`${serverURL}${mockImgTiendaPath}`).then((res) => res.json()).then((data) => printTarjetas(data));
+
+    //fetch(`${serverURL}${mockImgTiendaPath}`).then((res) => res.json()).then((data) => printTarjetas(data));
+    $.getJSON(`${serverURL}${mockImgTiendaPath}`, function (data, estado) {
+        if (estado == "success") {
+            printTarjetas(data)
+        } else {
+            alert("No se puedo acceder a la informacion")
+            window.location.href = "https://wwww.almapatisserie.com.ar"
+        }
+
+    })
 }
 
 function printTarjetas(data) { //IMPRIME LAS TARJETAS EN EL HTML
